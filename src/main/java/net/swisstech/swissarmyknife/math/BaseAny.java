123c5654b610d7461147bd544ca25645b5f5b607
Patrick Huber <stackmagic@gmail.com>
2014-10-13 23:52:30 +0200

    add base-any encoder/decoder

---
+++ src/main/java/net/swisstech/swissarmyknife/math/BaseAny.java
@@ -0,0 +1,71 @@
+package net.swisstech.swissarmyknife.math;
+
+/** encode/decode numbers into a set of symbols to shorten urls. think youtube and imgur url ids for example */
+public class BaseAny {
+
+	private final char[] chars;
+
+	public BaseAny(String chars) {
+		this.chars = chars.toCharArray();
+	}
+
+	public BaseAny(char[] chars) {
+		this.chars = chars.clone();
+	}
+
+	public String encode(double base, double value) {
+		StringBuilder sb = new StringBuilder();
+		encode(base, value, sb);
+		return sb.toString();
+	}
+
+	private void encode(double base, double value, StringBuilder sb) {
+		double div = value / base;
+		double mod = value % base;
+
+		if (div > 0) {
+			if (div > base) {
+				encode(base, div, sb);
+			}
+			else {
+				int idx = (int) div;
+				sb.append(chars[idx]);
+			}
+		}
+
+		if (mod < base) {
+			int idx = (int) mod;
+			sb.append(chars[idx]);
+			return;
+		}
+
+		encode(base, mod, sb);
+	}
+
+	public double decode(double base, String enc) {
+		return (decode(base, enc, 0d));
+	}
+
+	private double decode(double base, String enc, double d) {
+
+		char c = enc.charAt(0);
+
+		double val = 0;
+		for (int i = 0; i < base; i++) {
+			char cc = chars[i];
+			if (c == cc) {
+				val = i;
+			}
+		}
+
+		d = d * base + val;
+
+		if (enc.length() > 1) {
+			enc = enc.substring(1);
+			return decode(base, enc, d);
+		}
+		else {
+			return d;
+		}
+	}
+}
---
+++ src/test/java/net/swisstech/swissarmyknife/math/BaseAnyTest.java
@@ -0,0 +1,58 @@
+package net.swisstech.swissarmyknife.math;
+
+import static java.util.Arrays.asList;
+import static org.testng.Assert.assertEquals;
+
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+/** test the BaseAny */
+public class BaseAnyTest {
+
+	private final static String CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.-_~äöü!$;:^'?=)(/&%ç*\"+";
+
+	@Test
+	public void testEncodeDecode() {
+
+		BaseAny be = new BaseAny(CHARS);
+
+		double dbase = CHARS.length();
+		System.out.println("BASE   = " + dbase);
+		System.out.println("INT    = " + Integer.MAX_VALUE);
+		System.out.println("LONG   = " + Long.MAX_VALUE);
+		System.out.println("DOUBLE = " + Double.MAX_VALUE);
+		System.out.println("");
+
+		List<Double> values = asList( //
+			0d, //
+			1d, //
+			2d, //
+			dbase - 2d, //
+			dbase - 1d, //
+			dbase, //
+			dbase + 1d, //
+			dbase + 2d, //
+			128d, //
+			512d, //
+			1024d, //
+			Math.pow(2, 2) - 1d, //
+			Math.pow(2, 3) - 1d, //
+			Math.pow(2, 5) - 1d, //
+			Math.pow(2, 7) - 1d, //
+			Math.pow(2, 13) - 1d, //
+			Math.pow(2, 17) - 1d, //
+			Math.pow(2, 19) - 1d, //
+			Math.pow(2, 31) - 1d, // next mersenne prime (2^61-1 is out of range)
+			new Long(Long.MAX_VALUE).doubleValue()
+		// Double.MAX_VALUE ends up being 'Infinity'
+			);
+
+		for (Double value : values) {
+			String enc = be.encode(dbase, value);
+			double dec = be.decode(dbase, enc);
+			System.out.println(String.format("%40.0f => %20s => %40.0f => OK = %b", value, enc, dec, value.equals(dec)));
+			assertEquals(value, dec);
+		}
+	}
+}
