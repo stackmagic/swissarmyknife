4c7f5a513c653f0c190e5d6ad7e76e7179cc1616
Patrick Huber <stackmagic@gmail.com>
2014-11-04 17:26:21 +0100

    next to isBlank isNotBlank we now have blank/notBlank that will throw an IllegAlargumentException in case the expected state is violated

--- src/main/java/net/swisstech/swissarmyknife/lang/Strings.java
+++ src/main/java/net/swisstech/swissarmyknife/lang/Strings.java
@@ -10,8 +10,22 @@ public class Strings {
 		return s == null || s.trim().isEmpty();
 	}
 
+	public static String blank(String s) {
+		if (isNotBlank(s)) {
+			throw new IllegalArgumentException("String should be blank but got " + s);
+		}
+		return s;
+	}
+
 	public static boolean isNotBlank(String s) {
 		return !isBlank(s);
+	}
+
+	public static String notBlank(String s) {
+		if (isBlank(s)) {
+			throw new IllegalArgumentException("String shouldn't be empty but gut " + s);
+		}
+		return s;
 	}
 
 	public static String truncate(String string, int length) {
--- src/test/java/net/swisstech/swissarmyknife/lang/StringsTest.java
+++ src/test/java/net/swisstech/swissarmyknife/lang/StringsTest.java
@@ -1,7 +1,13 @@
 package net.swisstech.swissarmyknife.lang;
 
+import static net.swisstech.swissarmyknife.lang.Strings.blank;
+import static net.swisstech.swissarmyknife.lang.Strings.isBlank;
+import static net.swisstech.swissarmyknife.lang.Strings.isNotBlank;
+import static net.swisstech.swissarmyknife.lang.Strings.notBlank;
+import static net.swisstech.swissarmyknife.lang.Strings.truncate;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertTrue;
 
 import java.io.IOException;
@@ -19,37 +25,68 @@ public class StringsTest {
 	}
 
 	@Test
-	public void notBlank() {
-		assertFalse(Strings.isNotBlank(null));
-		assertFalse(Strings.isNotBlank(""));
-		assertFalse(Strings.isNotBlank(" "));
-		assertFalse(Strings.isNotBlank("    "));
-		assertTrue(Strings.isNotBlank(" a"));
-		assertTrue(Strings.isNotBlank("  a"));
-		assertTrue(Strings.isNotBlank("b "));
-		assertTrue(Strings.isNotBlank("b  "));
-		assertTrue(Strings.isNotBlank(" c "));
-		assertTrue(Strings.isNotBlank("  c  "));
+	public void isNotBlankOk() {
+		assertFalse(isNotBlank(null));
+		assertFalse(isNotBlank(""));
+		assertFalse(isNotBlank(" "));
+		assertFalse(isNotBlank("    "));
+		assertTrue(isNotBlank(" a"));
+		assertTrue(isNotBlank("  a"));
+		assertTrue(isNotBlank("b "));
+		assertTrue(isNotBlank("b  "));
+		assertTrue(isNotBlank(" c "));
+		assertTrue(isNotBlank("  c  "));
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void notBlankNull() {
+		notBlank(null);
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void notBlankEmpty() {
+		notBlank("");
 	}
 
 	@Test
-	public void blank() {
-		assertTrue(Strings.isBlank(null));
-		assertTrue(Strings.isBlank(""));
-		assertTrue(Strings.isBlank(" "));
-		assertTrue(Strings.isBlank("    "));
-		assertFalse(Strings.isBlank(" a"));
-		assertFalse(Strings.isBlank("  a"));
-		assertFalse(Strings.isBlank("b "));
-		assertFalse(Strings.isBlank("b  "));
-		assertFalse(Strings.isBlank(" c "));
-		assertFalse(Strings.isBlank("  c  "));
+	public void notBlankOk() {
+		notBlank("x");
 	}
 
 	@Test
-	public void truncate() {
+	public void isBlankOk() {
+		assertTrue(isBlank(null));
+		assertTrue(isBlank(""));
+		assertTrue(isBlank(" "));
+		assertTrue(isBlank("    "));
+		assertFalse(isBlank(" a"));
+		assertFalse(isBlank("  a"));
+		assertFalse(isBlank("b "));
+		assertFalse(isBlank("b  "));
+		assertFalse(isBlank(" c "));
+		assertFalse(isBlank("  c  "));
+	}
+
+	@Test
+	public void blankNullOk() {
+		assertNull(blank(null));
+	}
+
+	@Test
+	public void blankEmptyOk() {
+		assertEquals(blank(""), "");
+		assertEquals(blank("  "), "  ");
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void blankFail() {
+		blank("x");
+	}
+
+	@Test
+	public void truncateOk() {
 		String o = "hello world";
-		assertEquals(Strings.truncate(o, 5), "hello... (6 Bytes skipped)");
-		assertEquals(Strings.truncate(o, 15), o);
+		assertEquals(truncate(o, 5), "hello... (6 Bytes skipped)");
+		assertEquals(truncate(o, 15), o);
 	}
 }
