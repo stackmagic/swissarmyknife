3dd0642673aa6727d78c73727777b150289f704c
Patrick Huber <stackmagic@gmail.com>
2014-11-12 10:57:01 +0100

    some thread utils (sleepFor, sleepUntil)

---
+++ src/main/java/net/swisstech/swissarmyknife/lang/Threads.java
@@ -0,0 +1,35 @@
+package net.swisstech.swissarmyknife.lang;
+
+/**
+ * thread-related utils
+ * @since 1.1.5
+ */
+public final class Threads {
+
+	/** private constructor for utility class */
+	private Threads() {}
+
+	public static boolean sleepUntil(long end) {
+		return sleepFor(end - System.currentTimeMillis());
+	}
+
+	/**
+	 * Sleep for a certain amount of time and tell the caller whether or not to continue;
+	 * @param time The amount of time to sleep (in millis)
+	 * @return Returns <code>true</code> if the caller may continue, <code>false</code> if we were interrupted and the caller must stop running.
+	 */
+	public static boolean sleepFor(long time) {
+		long end = time + System.currentTimeMillis();
+		while (end > System.currentTimeMillis()) {
+			long remainder = end - System.currentTimeMillis();
+			try {
+				Thread.sleep(remainder);
+			}
+			catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+				return false;
+			}
+		}
+		return true;
+	}
+}
