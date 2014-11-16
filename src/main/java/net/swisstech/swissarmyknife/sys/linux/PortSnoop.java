cd90fbec41a0c5ea181bc99ec822a4e4acad778c
Patrick Huber <stackmagic@gmail.com>
2014-11-16 18:12:36 +0100

    better parsing of lsof output, add test for it

--- src/main/java/net/swisstech/swissarmyknife/sys/linux/PortSnoop.java
+++ src/main/java/net/swisstech/swissarmyknife/sys/linux/PortSnoop.java
@@ -21,6 +21,8 @@ public final class PortSnoop {
 
 	private static final Logger LOG = LoggerFactory.getLogger(PortSnoop.class);
 
+	private static final String NON_NUMBERS_REGEX = "[^0-9]+";
+
 	/** private constructor for utility class */
 	private PortSnoop() {}
 
@@ -29,19 +31,21 @@ public final class PortSnoop {
 
 		LOG.info("[%s] waiting until all of these ports are open: %s", process, ports);
 		long start = System.currentTimeMillis();
-		long end   = start + timeout;
+		long end = start + timeout;
 
 		// loop and sleep until all expected ports are open
 		while (true) {
 
 			// get what processes have opened the ports we're interested in
-			Map<Integer, Integer> portToPid = getPortToPid(ports);
+			Map<Integer, Set<Integer>> portToPid = getPortToPid(ports);
 			LOG.debug("[%d] Ports open right now with their pids: %s", process.getPid(), portToPid);
 
 			// check for multiple processes owning our ports
 			// TODO some programs may start subprocesses with new pids and we may allow for that
 			Set<Integer> pids = new HashSet<Integer>();
-			pids.addAll(portToPid.values());
+			for (Set<Integer> pid : portToPid.values()) {
+				pids.addAll(pid);
+			}
 
 			if (!pids.isEmpty() && pids.size() != 1) {
 				process.killWait(TERM);
@@ -85,16 +89,37 @@ public final class PortSnoop {
 	 * returns a map of the supplied ports mapping to the process pid owning the open port. if there's no entry for a supplied port in the map, there's no
 	 * process owning it (yet)
 	 */
-	public static Map<Integer, Integer> getPortToPid(Collection<Integer> ports) {
-		Map<Integer, Integer> map =new HashMap<Integer, Integer>();
+	public static Map<Integer, Set<Integer>> getPortToPid(Collection<Integer> ports) {
+		Map<Integer, Set<Integer>> map = new HashMap<Integer, Set<Integer>>();
 		for (Integer port : ports) {
 			String cmd = "lsof -ti :" + port;
 			String[] cmds = cmd.split(" ");
-			String pid = ProcessWrapper.launchWaitText(cmds, null);
-			if (isNotBlank(pid)) {
-				map.put(port, Integer.parseInt(pid.trim()));
+			String lsofStdOut = ProcessWrapper.launchWaitText(cmds, null);
+			if (isNotBlank(lsofStdOut)) {
+				Set<Integer> pids = map.get(port);
+				if (pids == null) {
+					pids = new HashSet<Integer>();
+					map.put(port, pids);
+				}
+
+				pids.addAll(parsePids(lsofStdOut));
 			}
 		}
 		return map;
+	}
+
+	/** parse the output of <code>lsof -ti :port</code>, essentially extracts all distinct numbers from a string, these are the pids in case of the lsof output. */
+	public static Set<Integer> parsePids(String lsofStdOut) {
+		Set<Integer> pids = new HashSet<Integer>();
+		String[] lines = lsofStdOut.split(NON_NUMBERS_REGEX);
+		for (String line : lines) {
+			if (line.length() < 1) {
+				continue;
+			}
+			Integer pid = Integer.parseInt(line);
+			pids.add(pid);
+		}
+
+		return pids;
 	}
 }
---
+++ src/test/java/net/swisstech/swissarmyknife/sys/linux/PortSnoopTest.java
@@ -0,0 +1,28 @@
+// Copyright (C) Layzapp AG. All Rights Reserved.
+package net.swisstech.swissarmyknife.sys.linux;
+
+import static net.swisstech.swissarmyknife.test.Assert.assertCollectionContainsAll;
+import static net.swisstech.swissarmyknife.test.Assert.assertEmpty;
+import static net.swisstech.swissarmyknife.util.Sets.newHashSet;
+
+import java.io.IOException;
+
+import net.swisstech.swissarmyknife.test.PrivateConstructor;
+
+import org.testng.annotations.Test;
+
+/** test the PortSnoop */
+public class PortSnoopTest {
+
+	@Test
+	public void privateConstructor() throws IOException {
+		PrivateConstructor.invoke(PortSnoop.class);
+	}
+
+	@Test
+	public void parsePids() {
+		assertEmpty(PortSnoop.parsePids(""));
+		assertEmpty(PortSnoop.parsePids("    \n   \t   abc"));
+		assertCollectionContainsAll(PortSnoop.parsePids(" 1   \n  2 \t  5 ab10c"), newHashSet(1, 2, 5, 10));
+	}
+}
