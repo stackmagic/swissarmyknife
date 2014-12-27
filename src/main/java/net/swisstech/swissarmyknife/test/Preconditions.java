67b4b31e0e384eef19a88e1dd2a862faf3c285bc
Patrick Huber <stackmagic@gmail.com>
2014-12-27 18:33:49 +0100

    deprecation

--- src/main/java/net/swisstech/swissarmyknife/test/Preconditions.java
+++ src/main/java/net/swisstech/swissarmyknife/test/Preconditions.java
@@ -11,9 +11,6 @@ import net.swisstech.swissarmyknife.lang.Strings;
  * class is intended for 'live' code, for unit tests, see {@link Assert}
  * @since 1.1.4
  */
-// will be replaced with methods without the ensure prefix plus everything should allow for an optional message to be appended (2 variations of each method at
-// least). Maybe also merge this with the Assert class and have a single fat class full of superduper validators
-@Deprecated
 public final class Preconditions {
 
 	private static final String EXPECTED = "expected ";
@@ -22,10 +19,13 @@ public final class Preconditions {
 	/** private constructor for utility class */
 	private Preconditions() {}
 
+	@Deprecated
+	/** @see {@link net.swisstech.swissarmyknife.lang.Strings#notBlank(java.lang.String)} */
 	public static String ensureNotEmpty(String s) {
 		return Strings.notBlank(s);
 	}
 
+	@Deprecated
 	public static <O> O ensureNull(O o, String msg) {
 		if (o != null) {
 			throw new IllegalArgumentException(msg);
@@ -37,6 +37,7 @@ public final class Preconditions {
 		return ensureNotNull(o, "Object must not be null");
 	}
 
+	@Deprecated
 	public static <O> O ensureNotNull(O o, String msg) {
 		if (o == null) {
 			throw new IllegalArgumentException(msg);
@@ -44,144 +45,168 @@ public final class Preconditions {
 		return o;
 	}
 
+	@Deprecated
 	public static void ensureTrue(boolean b, String msg) {
 		if (!b) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureFalse(boolean b, String msg) {
 		if (b) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureEquals(byte actual, byte expected, String msg) {
 		if (actual != expected) {
 			throw new IllegalArgumentException(EXPECTED + expected + BUT_GOT + actual + ": " + msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureEquals(short actual, short expected, String msg) {
 		if (actual != expected) {
 			throw new IllegalArgumentException(EXPECTED + expected + BUT_GOT + actual + ": " + msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureEquals(int actual, int expected, String msg) {
 		if (actual != expected) {
 			throw new IllegalArgumentException(EXPECTED + expected + BUT_GOT + actual + ": " + msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureEquals(long actual, long expected, String msg) {
 		if (actual != expected) {
 			throw new IllegalArgumentException(EXPECTED + expected + BUT_GOT + actual + ": " + msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureEquals(float actual, float expected, String msg) {
 		if (actual != expected) {
 			throw new IllegalArgumentException(EXPECTED + expected + BUT_GOT + actual + ": " + msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureEquals(double actual, double expected, String msg) {
 		if (actual != expected) {
 			throw new IllegalArgumentException(EXPECTED + expected + BUT_GOT + actual + ": " + msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureEquals(char actual, char expected, String msg) {
 		if (actual != expected) {
 			throw new IllegalArgumentException(EXPECTED + expected + BUT_GOT + actual + ": " + msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureEquals(boolean actual, boolean expected, String msg) {
 		if (actual != expected) {
 			throw new IllegalArgumentException(EXPECTED + expected + BUT_GOT + actual + ": " + msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureEquals(Object actual, Object expected, String msg) {
 		if (!actual.equals(expected)) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureLowerThanOrEqual(long value, long threshold, String msg) {
 		if (!(value <= threshold)) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureLowerThan(long value, long threshold, String msg) {
 		if (!(value < threshold)) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureHigherThan(long value, long threshold, String msg) {
 		if (!(value > threshold)) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureHigherThanOrEqual(long value, long threshold, String msg) {
 		if (!(value >= threshold)) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureHigherThanOrEqual(double value, double threshold, String msg) {
 		if (!(value >= threshold)) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureLowerThanOrEqual(double value, double threshold, String msg) {
 		if (!(value <= threshold)) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureLowerThan(double value, double threshold, String msg) {
 		if (!(value < threshold)) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureHigherThan(double value, double threshold, String msg) {
 		if (!(value > threshold)) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureBetweenIncluding(long lo, long hi, long value, String msg) {
 		if (value < lo || value > hi) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureNotEmpty(Collection<?> c, String msg) {
 		if (c.isEmpty()) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureEmpty(Collection<?> c, String msg) {
 		if (!c.isEmpty()) {
 			throw new IllegalArgumentException(msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureSize(Collection<?> c, int expectedSize, String msg) {
 		if (expectedSize != c.size()) {
 			throw new IllegalArgumentException("expected size was " + expectedSize + " but actual size was " + c.size() + ": " + msg);
 		}
 	}
 
+	@Deprecated
 	public static void ensureUrl(String url) {
 		ensureNotEmpty(url);
 		try {
