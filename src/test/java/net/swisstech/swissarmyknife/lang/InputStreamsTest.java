309c9711a8225dcb6c4f6196601d9791cb195f51
Patrick Huber <stackmagic@gmail.com>
2018-10-13 10:50:49 +0200

    Add InputStreams with waitForOutput method

---
+++ src/test/java/net/swisstech/swissarmyknife/lang/InputStreamsTest.java
@@ -0,0 +1,26 @@
+package net.swisstech.swissarmyknife.lang;
+
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputStreamsTest {
+
+	@Test
+	public void waitForOutputSuccess() throws IOException {
+		String needle = "hello world!";
+		String haystack = "lorem ipsum dolor sit amet he he hello world! from here on it should be ignored";
+		InputStream haystackStream = new ByteArrayInputStream(haystack.getBytes());
+		InputStreams.waitForOutput(needle, haystackStream, 100);
+	}
+
+	@Test(expectedExceptions = {IllegalStateException.class}, expectedExceptionsMessageRegExp = "Timeout while waiting .*")
+	public void waitForOutputTimeout() throws IOException {
+		String needle = "needle";
+		String haystack = "yeah the search term won't occur in this string so it'll timeout";
+		InputStream haystackStream = new ByteArrayInputStream(haystack.getBytes());
+		InputStreams.waitForOutput(needle, haystackStream, 100);
+	}
+}
