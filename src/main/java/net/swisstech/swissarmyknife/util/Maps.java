b1d8136225d1371d244dd42f3b4b45b4790adab9
Patrick Huber <stackmagic@gmail.com>
2019-03-17 23:06:00 +0100

    Maps: empty/notEmpty assertions and isNotEmpty query

--- src/main/java/net/swisstech/swissarmyknife/util/Maps.java
+++ src/main/java/net/swisstech/swissarmyknife/util/Maps.java
@@ -15,6 +15,35 @@ public final class Maps {
 	private Maps() {
 	}
 
+	/**
+	 * @since 2.1.0
+	 */
+	public static <K, V> Map<K, V> empty(Map<K, V> map) {
+		if (isNotEmpty(map)) {
+			throw new IllegalArgumentException("Expected map to be empty but it isn't");
+		}
+
+		return map;
+	}
+
+	/**
+	 * @since 2.1.0
+	 */
+	public static <K, V> Map<K, V> notEmpty(Map<K, V> map) {
+		if (isEmpty(map)) {
+			throw new IllegalArgumentException("Expected map to not be empty but it is");
+		}
+
+		return map;
+	}
+
+	/**
+	 * @since 2.1.0
+	 */
+	public static <K, V> boolean isNotEmpty(Map<K, V> map) {
+		return !isEmpty(map);
+	}
+
 	public static <K, V> boolean isEmpty(Map<K, V> map) {
 		return map == null || map.isEmpty();
 	}
--- src/test/java/net/swisstech/swissarmyknife/util/MapsTest.java
+++ src/test/java/net/swisstech/swissarmyknife/util/MapsTest.java
@@ -5,19 +5,67 @@ import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
-import static net.swisstech.swissarmyknife.util.Maps.isEmpty;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
+import static net.swisstech.swissarmyknife.util.Maps.*;
+import static org.testng.Assert.*;
 
 /**
  * test the Maps
  */
 public class MapsTest {
 
+	private static final Map<String, String> EMPTY = new HashMap<>();
+	private static final Map<String, String> MAP = new MapBuilder<>("Hello", "World").build();
+
 	@Test
 	public void privateConstructor() throws IOException {
 		PrivateConstructor.invoke(Maps.class);
+	}
+
+	@Test()
+	public void emptyNull() {
+		assertNull(empty(null));
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Expected map to be empty but it isn't")
+	public void emptyNotEmpty() {
+		empty(MAP);
+	}
+
+	@Test
+	public void emptyEmpty() {
+		assertSame(empty(EMPTY), EMPTY);
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Expected map to not be empty but it is")
+	public void notEmptyNull() {
+		notEmpty(null);
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Expected map to not be empty but it is")
+	public void notEmptyEmpty() {
+		notEmpty(EMPTY);
+	}
+
+	@Test
+	public void notEmptyFilled() {
+		assertSame(notEmpty(MAP), MAP);
+	}
+
+	@Test
+	public void isNotEmptyNull() {
+		assertFalse(isNotEmpty(null));
+	}
+
+	@Test
+	public void isNotEmptyEmpty() {
+		assertFalse(isNotEmpty(EMPTY));
+	}
+
+	@Test
+	public void isNotEmptyFilled() {
+		assertTrue(isNotEmpty(MAP));
 	}
 
 	@Test
@@ -27,11 +75,11 @@ public class MapsTest {
 
 	@Test
 	public void isEmptyEmpty() {
-		assertTrue(isEmpty(new HashMap<String, String>()));
+		assertTrue(isEmpty(EMPTY));
 	}
 
 	@Test
 	public void isEmptyFilled() {
-		assertFalse(isEmpty(new MapBuilder<String, String>("Hello", "World").build()));
+		assertFalse(isEmpty(MAP));
 	}
 }
