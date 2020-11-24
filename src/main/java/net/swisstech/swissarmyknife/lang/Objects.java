b0bfc149062336e0e608b274b082b4d533e735ce
Patrick Huber <stackmagic@gmail.com>
2020-11-24 23:04:04 +0100

    Objects: valueOrDefault

--- src/main/java/net/swisstech/swissarmyknife/lang/Objects.java
+++ src/main/java/net/swisstech/swissarmyknife/lang/Objects.java
@@ -2,13 +2,21 @@ package net.swisstech.swissarmyknife.lang;
 
 public final class Objects {
 
-	/** private constructor for utility class */
-	private Objects() {}
+    /** private constructor for utility class */
+    private Objects() {}
 
-	public static <T> T notNull(T o) {
-		if (o == null) {
-			throw new IllegalArgumentException("Object must not be null");
-		}
-		return o;
-	}
+    public static <T> T notNull(T o) {
+        if (o == null) {
+            throw new IllegalArgumentException("Object must not be null");
+        }
+        return o;
+    }
+
+    /** @since 2.1.0 */
+    public static <T> T valueOrDefault(T value, T dflt) {
+        if (value == null) {
+            return dflt;
+        }
+        return value;
+    }
 }
--- src/test/java/net/swisstech/swissarmyknife/lang/ObjectsTest.java
+++ src/test/java/net/swisstech/swissarmyknife/lang/ObjectsTest.java
@@ -1,28 +1,35 @@
 package net.swisstech.swissarmyknife.lang;
 
-import static org.testng.Assert.assertSame;
+import net.swisstech.swissarmyknife.test.PrivateConstructor;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-import net.swisstech.swissarmyknife.test.PrivateConstructor;
-
-import org.testng.annotations.Test;
+import static org.testng.Assert.assertSame;
 
 public class ObjectsTest {
 
-	@Test
-	public void privateConstructor() throws IOException {
-		PrivateConstructor.invoke(Objects.class);
-	}
+    @Test
+    public void privateConstructor() throws IOException {
+        PrivateConstructor.invoke(Objects.class);
+    }
 
-	@Test(expectedExceptions = IllegalArgumentException.class)
-	public void notNullNull() {
-		Objects.notNull((Object) null);
-	}
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void notNullNull() {
+        Objects.notNull((Object) null);
+    }
 
-	@Test
-	public void notNull() {
-		Object o = new Object();
-		assertSame(Objects.notNull(o), o);
-	}
+    @Test
+    public void notNull() {
+        Object o = new Object();
+        assertSame(Objects.notNull(o), o);
+    }
+
+    @Test
+    public void valueOfDefault() {
+        String x = "hello test";
+        assertSame(Objects.valueOrDefault(null, x), x);
+        assertSame(Objects.valueOrDefault(x, null), x);
+        assertSame(Objects.valueOrDefault(x, "nope"), x);
+    }
 }
