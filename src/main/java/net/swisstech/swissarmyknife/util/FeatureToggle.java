c6c7e61bfdaf22bba6206fa46e2f36ed4424d2ba
Patrick Huber <stackmagic@gmail.com>
2021-06-05 20:07:41 +0200

    Make FeatureToggle better, adds checking, things like enableAll/disableAll etc

--- src/main/java/net/swisstech/swissarmyknife/util/FeatureToggle.java
+++ src/main/java/net/swisstech/swissarmyknife/util/FeatureToggle.java
@@ -1,19 +1,20 @@
 package net.swisstech.swissarmyknife.util;
 
+import net.swisstech.swissarmyknife.test.Preconditions;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Disable the ability to call certain methods. By default, all methods are
  * permitted and need to be disabled by calling disable(methodName).
  *
  * todo verify the passed class objects are actually interfaces and enumerate all
- *  methods in a Map<MethodName, EnabledDisabledStatus> so we can do things like
- *  enableAll/disableAll
+ * methods in a Map<MethodName, EnabledDisabledStatus> so we can do things like
+ * enableAll/disableAll
  *
  * @param <T> Type of the wrapped object
  */
@@ -22,28 +23,36 @@ public class FeatureToggle<T> {
     /** proxy/wrapper on which we intercept calls */
     private final T proxy;
 
-    /** set of disabled methods */
-    private final Set<String> disabledMethodNames = new HashSet<>();
+    /** enable/disable state of the methods */
+    private final Map<String, MethodStatus> methods = new HashMap<>();
+
+    /** string representation of proxies interfaces */
+    private final String interfacesString;
 
     /**
      * @param instance       The object to wrap/protect - retrieve the proxy for it by calling getProxy()
      * @param interfaceClass The interface Class for which we will create the proxy
      */
-    public FeatureToggle(T instance, Class<? super T> interfaceClass) {
+    public FeatureToggle(T instance, MethodStatus defaultStatus, Class<? super T> interfaceClass) {
         //noinspection unchecked
-        this(instance, new Class[]{interfaceClass});
+        this(instance, defaultStatus, new Class[]{interfaceClass});
     }
 
     /**
-     * @param instance         The object to wrap/protect - retrieve the proxy for it by calling getProxy()
-     * @param interfaceClasses The interface Classes for which we will create the proxy
+     * @param instance   The object to wrap/protect - retrieve the proxy for it by calling getProxy()
+     * @param interfaces The interface Classes for which we will create the proxy
      */
-    public FeatureToggle(T instance, Class<? super T>[] interfaceClasses) {
+    public FeatureToggle(T instance, MethodStatus defaultStatus, Class<? super T>[] interfaces) {
+        interfacesString = Arrays.stream(interfaces)
+                .peek(Preconditions::interfacee)
+                .peek(it -> Arrays.stream(it.getMethods()).forEach(m -> methods.put(m.getName(), defaultStatus)))
+                .map(Class::getSimpleName).collect(Collectors.joining(" "));
+
         //noinspection unchecked
         this.proxy = (T) Proxy.newProxyInstance(
                 FeatureToggle.class.getClassLoader(),
-                interfaceClasses,
-                new FeatureToggleInvocationHandler(instance, disabledMethodNames)
+                interfaces,
+                new FeatureToggleInvocationHandler(instance, methods)
         );
     }
 
@@ -51,39 +60,73 @@ public class FeatureToggle<T> {
         return proxy;
     }
 
-    public Set<String> getDisabledMethodNames() {
-        return Collections.unmodifiableSet(disabledMethodNames);
+    public boolean isEnabled(String methodName) {
+        return MethodStatus.ENABLED == getMethodStatus(methodName);
+    }
+
+    public boolean isDisabled(String methodName) {
+        return MethodStatus.DISABLED == getMethodStatus(methodName);
+    }
+
+    public MethodStatus getMethodStatus(String methodName) {
+        return methods.get(methodName);
+    }
+
+    public Map<String, MethodStatus> getMethodStatuses() {
+        return Collections.unmodifiableMap(methods);
     }
 
     public void enable(String methodName) {
-        disabledMethodNames.remove(methodName);
+        setStatus(methodName, MethodStatus.ENABLED);
     }
 
     public void disable(String methodName) {
-        disabledMethodNames.add(methodName);
+        setStatus(methodName, MethodStatus.DISABLED);
+    }
+
+    public void enableAll() {
+        methods.keySet().forEach(this::enable);
+    }
+
+    public void disableAll() {
+        methods.keySet().forEach(this::disable);
+    }
+
+    public void setStatus(String methodName, MethodStatus status) {
+        if (!methods.containsKey(methodName)) {
+            throw new IllegalArgumentException("Method " + methodName + " does not exist on interfaces " + interfacesString);
+        }
+        methods.put(methodName, Objects.requireNonNull(status));
     }
 
     /** inner class so one can't use "FeatureToggle.invoke" */
     private static class FeatureToggleInvocationHandler implements InvocationHandler {
 
         private final Object instance;
-        private final Set<String> disabledMethodNames;
+        private final Map<String, MethodStatus> methods;
 
-        public FeatureToggleInvocationHandler(Object instance, Set<String> disabledMethodNames) {
+        public FeatureToggleInvocationHandler(Object instance, Map<String, MethodStatus> methods) {
             this.instance = instance;
-            this.disabledMethodNames = disabledMethodNames;
+            this.methods = methods;
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             String methodName = method.getName();
-            boolean isMethodDisabled = disabledMethodNames.contains(methodName);
+            MethodStatus status = methods.get(methodName);
 
-            if (isMethodDisabled) {
-                throw new UnsupportedOperationException("Calling of " + methodName + " is disabled");
+            switch (status) {
+                case ENABLED:
+                    return method.invoke(instance, args);
+                case DISABLED:
+                default:
+                    throw new UnsupportedOperationException("Calling of " + methodName + " is disabled");
             }
-
-            return method.invoke(instance, args);
         }
+    }
+
+    public enum MethodStatus {
+        ENABLED,
+        DISABLED
     }
 }
--- src/test/java/net/swisstech/swissarmyknife/util/FeatureToggleTest.java
+++ src/test/java/net/swisstech/swissarmyknife/util/FeatureToggleTest.java
@@ -2,20 +2,25 @@ package net.swisstech.swissarmyknife.util;
 
 import org.testng.annotations.Test;
 
-import java.util.HashSet;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Set;
+import java.util.TreeSet;
 
-import static net.swisstech.swissarmyknife.test.Assert.*;
+import static net.swisstech.swissarmyknife.test.Assert.assertCollectionContains;
+import static net.swisstech.swissarmyknife.test.Assert.assertEmpty;
+import static org.testng.Assert.*;
 
 public class FeatureToggleTest {
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
     @Test
-    public void testSet() {
-        Set<String> subject = new HashSet<>();
-        FeatureToggle<Set<String>> featureToggle = new FeatureToggle<>(subject, Set.class);
+    public void testSet() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Set<String> subject = new TreeSet<>();
+        FeatureToggle<Set<String>> featureToggle = new FeatureToggle<>(subject, FeatureToggle.MethodStatus.ENABLED, Set.class);
         Set<String> proxy = featureToggle.getProxy();
-        assertEmpty(featureToggle.getDisabledMethodNames());
+
+        assertTrue(featureToggle.isEnabled("contains"));
+        assertFalse(featureToggle.isDisabled("contains"));
 
         // stuff is enabled by default
         proxy.add("test");
@@ -23,7 +28,8 @@ public class FeatureToggleTest {
 
         // disable method
         featureToggle.disable("contains");
-        assertSize(featureToggle.getDisabledMethodNames(), 1);
+        assertTrue(featureToggle.isDisabled("contains"));
+        assertFalse(featureToggle.isEnabled("contains"));
         subject.contains("test");
         try {
             proxy.contains("test");
@@ -33,17 +39,39 @@ public class FeatureToggleTest {
 
         // disable method
         featureToggle.disable("remove");
-        assertSize(featureToggle.getDisabledMethodNames(), 2);
+
+        assertTrue(featureToggle.isDisabled("contains"));
+        assertFalse(featureToggle.isEnabled("contains"));
+        assertTrue(featureToggle.isDisabled("remove"));
+        assertFalse(featureToggle.isEnabled("remove"));
+
         try {
             proxy.remove("test");
         } catch (UnsupportedOperationException e) {
             // ok
         }
 
+        // check status another way
+        assertEquals(featureToggle.getMethodStatuses().values().stream().filter(it -> it == FeatureToggle.MethodStatus.DISABLED).count(), 2);
+
         // re-enable method
-        assertCollectionContains(subject, "test");
-        featureToggle.enable("remove");
+        featureToggle.enableAll();
+
+        assertTrue(proxy.contains("test"));
         proxy.remove("test");
+        assertFalse(proxy.contains("test"));
         assertEmpty(subject);
+
+        // disableAll
+        featureToggle.disableAll();
+        assertEquals(featureToggle.getMethodStatuses().values().stream().filter(it -> it == FeatureToggle.MethodStatus.ENABLED).count(), 0);
+        assertEquals(featureToggle.getMethodStatuses().values().stream().filter(it -> it == FeatureToggle.MethodStatus.DISABLED).count(), featureToggle.getMethodStatuses().size());
+
+        // set status of invalid method
+        try {
+            featureToggle.setStatus("blargh", FeatureToggle.MethodStatus.ENABLED);
+        } catch (IllegalArgumentException e) {
+            // ok
+        }
     }
 }
